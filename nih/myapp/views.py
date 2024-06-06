d1f749d3f1bd9ad2906f8c635fd99c8ea416d584
Habb <ihabhasanainakmal0409@gmail.com>
2024-06-06 15:07:12 +0700

    Your commit message

---
+++ nih/myapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from .forms import SignupForm
+from django.contrib import messages
+
+# Create your views here.
+# Home page
+def index(request):
+    return render(request, 'index.html')
+
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid email or password.')
+            return redirect('/auth/login/')
+    return render(request, 'login.html')
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
